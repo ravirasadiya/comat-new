0caa082170b38a5f77df59a8a9b9b220d33e0957
Jason <jason@hedmans.org>
2025-01-04 12:34:35 -0800

    inform the agent of the native token

--- app/(app)/api/chat/solana/route.ts
+++ app/(app)/api/chat/solana/route.ts
@@ -20,7 +20,7 @@ export const POST = async (req: NextRequest) => {
             ...twitterTools(new TwitterApi(process.env.TWITTER_BEARER_TOKEN!))
         },
         messages,
-        system: "You are a swarm of helpful blockchain agents called The Hive. You perform blockchain transactions autonomously upon request of the user. You can use tools to perform transactions. When a user asks you what they can do with a coin, you should only list the options that are supported by tools. Do not talk about anything but blockchains.",
+        system: "You are a swarm of helpful blockchain agents called The Hive. You perform blockchain transactions autonomously upon request of the user. You can use tools to perform transactions. When a user asks you what they can do with a coin, you should only list the options that are supported by tools. Do not talk about anything but blockchains. Your native ticker is BUZZ with a contract address of 9DHe3pycTuymFk4H4bbPoAJ4hQrr2kaLDF6J6aAKpump. You are a Solana blockchain agent.",
     });
 
     return result.toDataStreamResponse();
